840f5b1becfa7a1fa6cee91e4afada74dc1d8ca3
liujinhu0511 <960079973@qq.com>
2020-06-24 16:56:02 +0800

    提交首页的页面布局和功能

--- assets/js/commen.js
+++ assets/js/commen.js
@@ -1,6 +1,18 @@
 $(function () {
   $.ajaxPrefilter(function (options) {
     // 配置统一url
-    options.url = 'http://www.liulongbin.top:3007' + options.url;
-  })
-})
+    options.url = "http://www.liulongbin.top:3007" + options.url;
+    // 配置统一headers
+    if (options.url.indexOf("/my/") !== -1) {
+      options.headers = {
+        Authorization: localStorage.getItem("token"),
+      };
+      options.complete = function (res) {
+        if (res.responseJSON.status === 1 && res.responseJSON.message === "身份认证失败！") {
+          localStorage.removeItem("token");
+          location.href = "/login.html";
+        }
+      };
+    }
+  });
+});
---
+++ assets/js/index.js
@@ -0,0 +1,34 @@
+$(function () {
+  // 加载用户基本信息
+  getUserInfo();
+  // 退出功能
+  $("#btn-exit").click(function () {
+    // 关闭当前页面
+    // 删除token
+    layer.confirm("你确定要退出吗？", { icon: 3, title: "提示" }, function (index) {
+      localStorage.removeItem("token");
+      location.href = "/login.html";
+      layer.close(index);
+    });
+  });
+});
+
+// 加载用户基本信息
+function getUserInfo() {
+  $.ajax({
+    url: "/my/userinfo",
+    success: function (res) {
+      if (res.status === 0) {
+        var name = res.data.nickname || res.data.username;
+        if (res.user_pic) {
+          $(".layui-nav-img").attr("src", res.data.user_pic).show();
+          $(".img-text").hide();
+        } else {
+          $(".img-text").css("display", "inline-block").html(name.substr(0, 1).toUpperCase());
+          $(".layui-nav-img").hide();
+        }
+        $("#weclome").html("欢迎&nbsp;&nbsp;" + name);
+      }
+    },
+  });
+}
